e2c5d52e18a79b268ca98068e65056b15151d739
root <allknowingwizard@gmail.com>
2018-04-06 16:29:03 -0400

    howdy

--- cpp/Assignment 6/ice_cream_ars.cpp
+++ cpp/Assignment 6/ice_cream_ars.cpp
@@ -27,13 +27,14 @@ enum MENU_OPTION {ADD=1, LIST, SEARCH, TOTAL, MODIFY, CLEAR, EXIT, COUNT_P1};
 
 void displayFlavourData(IceCream &); // reads the next IceCream data from the file and prints it on screen
 void modifyItem(fstream &, bool); // modifies the next IceCream item in the file if bool if false, otherwise appends a new IceCream item
-void listFlavours(fstream &); // reads all of the IceCream items from the file and prints the data on the screen
+int listFlavours(fstream &, bool); // reads all of the IceCream items from the file and prints the data on the screen, as solection list as specified
+				   // returns the amount of flavours read
 void displayStockValue(fstream &); // calculates the monetary value of all on hand IceCream and prints it to the screen
 int displayPrompt(const string[], int, string &); // displays the contents of the array, of the given size, as a menu and prompts user for input stored in string
 										  // returns value based on input type (0 => string, 1 => int, -1 => combo or failure)
 void clearFile(fstream &, string); // clears the content of the file; destroys the stock record
 void searchFlavours(fstream &); // prompts the user for an item search
-void showStreamState(fstream &);
+void showStreamState(fstream &); // debugs the state of the given file stream
 
 
 class Logger { // experimenting with "static classes", I may add file based logging to this later
@@ -75,7 +76,7 @@ int main() {
 					modifyItem(file, true);
 					break;
 				case LIST: // list available flavours
-					listFlavours(file);
+					listFlavours(file, false);
 					break;
 				case SEARCH: // search flavours
 					searchFlavours(file);
@@ -107,29 +108,48 @@ int main() {
 
 void displayFlavourData(IceCream &tempFlav) {
 		cout << "--------------------------------------------------------\n"; // and print the info
-		cout << "Flavour: " << tempFlav.flavour << endl;
-		cout << "Quantity Available: " << tempFlav.quantity << endl;
-		cout << "Price: $" << fixed << setprecision(2) << tempFlav.price << endl;
+		cout << left << setw(25) << "Flavour: " << tempFlav.flavour << endl;
+		cout << left << setw(25) << "Quantity Available: " << tempFlav.quantity << endl;
+		cout << left << setw(25) << "Price: $" << fixed << setprecision(2) << tempFlav.price << endl;
 }
 
 void modifyItem(fstream &file, bool newF) {
-	cout << "Please Enter the Information.";
-	file.seekp(0, ios::beg);
-	file.seekg(0, ios::beg);
-	IceCream tempFlav = {"", 0, 0};
+	//print flavour list
+	string choice[] = {"Please Selected a Flavour Number"};
+	string input;
+	int value = -5;
 	if(!newF) {
-		cout << "Leave empty to remain unchanged.";
+		int count = listFlavours(file, true);
+		if(count > 0) {
+			while(value != 1) {
+				value = displayPrompt(choice, 0, input);
+				if(value == 1) {
+					file.seekg((stoi(input)-1) * sizeof(IceCream), ios::beg);
+					file.seekp((stoi(input)-1) * sizeof(IceCream), ios::beg);
+				} else {
+					cout << IN_ERROR;
+				}
+			}
+		} else {
+			cout << "No existing files. Adding new entry." << endl;
+			file.seekp(0, ios::end);
+			newF = true;
+		}
+	} else {
+		file.seekp(0, ios::end);
+	}
+	file.clear();
+	IceCream tempFlav = {0};
+	cout << "Please Enter the Information." << endl;
+	if(!newF) {
+		cout << "Return to leave unchanged." << endl;
 		file.read(reinterpret_cast<char*>(&tempFlav), sizeof(IceCream));///////////////////////////////////////////////////////////////
 		file.seekg(-sizeof(IceCream), ios::cur); // back up one item (the one you just read)
-	} else {
-		//file.seekg(0L, ios::end);
 	}
-	cout << endl;
 	string flavour[] = {"Flavour Name"};
 	string quantity[] = {"Quantity (lbs.)"};
 	string price[] = {"Price"};
-	string input = "";
-	int value = -1;
+	value = -5;
 	while(!(value == 0 || value == -2)) { // wait for string or empty input
 		value = displayPrompt(flavour, 0, input);
 		if(value == 0) { //  change the name
@@ -151,22 +171,29 @@ void modifyItem(fstream &file, bool newF) {
 		}
 	}
 	value = -5; // reset for continued input
-//	if(tempFlav != {"", 0, 0}) { // make sure there is information
-		file.write(reinterpret_cast<char *>(&tempFlav), sizeof(IceCream));
-//	}
+	
+	file.write(reinterpret_cast<char *>(&tempFlav), sizeof(IceCream));
 }
 
-void listFlavours(fstream &file) {
+int listFlavours(fstream &file, bool selList) {
+	file.clear();
 	file.seekg(0, ios::beg); // start reading from the beginning
 	showStreamState(file);
 	IceCream tempFlav = {"TestFlavour", 0, 0};
+	int iter = 0;
 	while(file.read(reinterpret_cast<char *>(&tempFlav), sizeof(IceCream))) { // read all of the flavor data
-		displayFlavourData(tempFlav);
+		if(!selList) {
+			displayFlavourData(tempFlav);
+		} else {
+			cout << right << setw(3) << (iter+1) << ": " << tempFlav.flavour << endl;
+		}
+		iter++;
 	}
-	file.clear();
+	return iter;
 }
 
 void displayStockValue(fstream &file) {
+	file.clear();
 	file.seekg(0, ios::beg); // reset to the beginning of the file
 	IceCream temp = {"", 0, 0};
 	float stockValue = 0;
@@ -195,7 +222,7 @@ int displayPrompt(const string options[], int nOptions, string &input) {
 		if(!(isdigit(input[i]) || input[i] == '-' || input[i] == '.')) { // check each character for digit and alpha
 			digit = false;
 		}
-		if(!(isalpha(input[i]))) {
+		if(!(isalpha(input[i]) || input[i] == ' ' || input[i] == '.')) {
 			alpha = false;
 		}
 	}
@@ -207,19 +234,61 @@ int displayPrompt(const string options[], int nOptions, string &input) {
 }
 
 void searchFlavours(fstream &file) {
-/*
-	string promptQuery[] = {"Enter a search term"};
+	file.clear();
+	string promptQuery[] = {"Enter a search term (-1 to exit)"};
 	int val = -5;
 	string input = "";
+	IceCream tempFlav = {0};
+	file.seekg(0, ios::beg);
+	bool resultFound = false;
+	bool c1, c2 = false;
+	char ch1[NAME_SIZE];
+	char ch2[NAME_SIZE];
 	while (val != 0) {
 		val = displayPrompt(promptQuery, 0, input);
 		if(val == 0) {
-			while
+			while(file.read(reinterpret_cast<char *>(&tempFlav), sizeof(IceCream))) {
+				int i = 0;
+				strcpy(ch1, tempFlav.flavour);
+				strcpy(ch2, input.c_str());
+				c1 = c2 = false;
+				while(!(c1 || c2)) {
+					if(!c1) {
+						if(ch1[i] == '\0')
+							c1 = true;
+						ch1[i] = tolower(ch1[i]);
+					}
+					if(!c2) {
+						if(ch2[i] == '\0')
+							c2 = true;
+						ch2[i] = tolower(ch2[i]);
+					}
+					i++;
+				}
+				
+				cout << "ch1: " << ch1 << endl;
+				cout << "ch2: " << ch2 << endl;
+				
+				if(strstr(ch1, ch2) != nullptr) {
+					displayFlavourData(tempFlav);
+					resultFound = true;
+				}
+			}
+			if(!resultFound) {
+				cout << "No results found matching that query.\n\n";
+				val = -5; // reset the loop to ask for more input
+			}
+		} else if(val == 1) {
+			if(stoi(input) == -1) { // -1 has been entered
+				cout << "Returning to main menu.\n\n";
+			} else { // not a valid number
+				cout << IN_ERROR; // prompt error
+				val = -5; // reset loop
+			}
 		} else {
 			cout << IN_ERROR; // wrong data type
 		}
 	}
-*/
 }
 
 void showStreamState(fstream &file) {
@@ -230,10 +299,12 @@ void showStreamState(fstream &file) {
 }
 
 void clearFile(fstream &file, string fileName) {
+	file.clear();
 	file.close();
 	file.open(fileName.c_str(), ios::out | ios::binary);
 	file.close();
 	file.open(fileName.c_str(), ios::out | ios::in | ios::app | ios::binary);
 	if(DEBUG)
 		showStreamState(file);
+	file.clear();
 }
