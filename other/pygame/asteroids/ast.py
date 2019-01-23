648113c251a23f6384faa8b3b22ba22fe8098588
Stammer <is2751zv@winona.edu>
2019-01-23 17:30:42 -0600

    pong to desktop

--- other/pygame/asteroids/ast.py
+++ other/pygame/asteroids/ast.py
@@ -1,8 +1,8 @@
-import sys, pygame
+import sys, pygame, time
 pygame.init()
 
-size = width, height = 320, 240
-speed = [2, 2]
+size = width, height = 800, 500
+speed = [3, 3]
 black = 0, 0, 0
 
 screen = pygame.display.set_mode(size)
@@ -11,6 +11,7 @@ ball = pygame.image.load("ball.png")
 ballrect = ball.get_rect()
 
 while 1:
+    time.sleep(.01)
     for event in pygame.event.get():
         if event.type == pygame.QUIT: sys.exit()
 
@@ -23,3 +24,11 @@ while 1:
     screen.fill(black)
     screen.blit(ball,ballrect)
     pygame.display.flip()
+def changePosition(nX, nY):
+    ballrect.x = nX;
+    ballrect.y = nY;
+def receivePositionThread():
+    while True:
+        data, addr = sock.recvfrom(1024) # buffer size is 1024 bytes
+        pos=data.decode('utf-8')).split(',')
+        changePosition(pos[0],pos[1])
