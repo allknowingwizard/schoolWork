526ec8b5fc552b9a99b905aa711aa3e060dd95c8
Stammer <is2751zv@winona.edu>
2019-04-28 10:10:10 -0500

    sql scripts

---
+++ sql/termProj/formatted/scripts/addStates.sql
@@ -0,0 +1,111 @@
+use Census;
+
+INSERT INTO state
+VALUE ('Alabama'); 
+INSERT INTO state
+VALUE ('Alaska');
+INSERT INTO state
+VALUE ( 'Arizona');
+INSERT INTO state
+VALUE ('Arkansas'); 
+INSERT INTO state
+VALUE ('California'); 
+
+INSERT INTO state
+VALUE ('Colorado'); 
+INSERT INTO state
+VALUE ('Connecticut');
+INSERT INTO state
+VALUE ( 'Delaware'); 
+INSERT INTO state
+VALUE ('Florida');
+INSERT INTO state
+VALUE ('Georgia'); 
+
+INSERT INTO state
+VALUE ('Hawaii'); 
+INSERT INTO state
+VALUE ('Idaho');
+INSERT INTO state
+VALUE ('Illinois');
+INSERT INTO state
+VALUE ( 'Indiana'); 
+INSERT INTO state
+VALUE ('Iowa');
+INSERT INTO state
+VALUE ('Kansas');
+
+INSERT INTO state
+VALUE ('Kentucky');
+INSERT INTO state
+VALUE ( 'Louisiana'); 
+INSERT INTO state
+VALUE ('Maine');
+INSERT INTO state
+VALUE ('Maryland');
+INSERT INTO state
+VALUE ('Massachusetts'); 
+
+INSERT INTO state
+VALUE ('Michigan');
+INSERT INTO state
+VALUE ('Minnesota');
+INSERT INTO state
+VALUE ('Mississippi'); 
+INSERT INTO state
+VALUE ('Missouri'); 
+INSERT INTO state
+VALUE ('Montana');
+
+INSERT INTO state
+VALUE ('Nebraska'); 
+INSERT INTO state
+VALUE ('Nevada');
+INSERT INTO state
+VALUE ( 'New Hampshire'); 
+INSERT INTO state
+VALUE ('New Jersey');
+INSERT INTO state
+VALUE ('New Mexico');
+
+INSERT INTO state
+VALUE ('New York');
+INSERT INTO state
+VALUE ('North Carolina');
+INSERT INTO state
+VALUE ( 'North Dakota');
+INSERT INTO state
+VALUE ( 'Ohio');
+INSERT INTO state
+VALUE ('Oklahoma');
+
+INSERT INTO state
+VALUE ('Oregon');
+INSERT INTO state
+VALUE ( 'Pennsylvania');
+INSERT INTO state
+VALUE ( 'Rhode Island');
+INSERT INTO state
+VALUE ('South Carolina'); 
+
+INSERT INTO state
+VALUE ('South Dakota'); 
+INSERT INTO state
+VALUE ('Tennesse'); 
+INSERT INTO state
+VALUE ('Taxas'); 
+INSERT INTO state
+VALUE ('Utah');
+INSERT INTO state
+VALUE ('Vermont');
+INSERT INTO state
+VALUE ('Virginia');
+
+INSERT INTO state
+VALUE ('Washington'); 
+INSERT INTO state
+VALUE ('West Verginia'); 
+INSERT INTO state
+VALUE ('Wisconsin'); 
+INSERT INTO state
+VALUE ('Wyoming');
---
+++ sql/termProj/formatted/scripts/createDatabase.sql
@@ -0,0 +1,50 @@
+use census;
+
+create table if not exists state(
+   state_name VARCHAR(40),
+   PRIMARY KEY ( state_name )
+);
+
+create table if not exists region(
+	region_name VARCHAR(40) NOT NULL,
+    PRIMARY KEY (region_name)
+);
+
+create table if not exists city(
+	city_name VARCHAR(40) NOT NULL,
+    PRIMARY KEY (city_name)
+);
+
+create table if not exists year(
+	year INT NOT NULL,
+    PRIMARY KEY(year)
+);
+
+create table if not exists state_measure(
+	state_name VARCHAR(40),
+    population INT,
+    year INT,
+    number_of_reps INT,
+    PRIMARY KEY (state_name, year),
+    FOREIGN KEY (state_name) REFERENCES state(state_name),
+    FOREIGN KEY (year) REFERENCES year(year)
+);
+create table if not exists city_measure(
+	state_name VARCHAR(40) NOT NULL,
+    city_name VARCHAR(40) NOT NULL,
+    population INT NOT NULL,
+    year INT NOT NULL,
+    PRIMARY KEY (state_name, city_name, year),
+    FOREIGN KEY (state_name) REFERENCES state(state_name),
+    FOREIGN KEY (city_name) REFERENCES city(city_name),
+    FOREIGN KEY (year) REFERENCES year(year)
+);
+create table if not exists region_measure(
+	region_name VARCHAR(40) NOT NULL,
+    population INT NOT NULL,
+    year INT NOT NULL,
+    number_of_reps INT NOT NULL,
+    PRIMARY KEY (region_name, year),
+    FOREIGN KEY (region_name) REFERENCES region(region_name),
+    FOREIGN KEY (year) REFERENCES year(year)
+);
